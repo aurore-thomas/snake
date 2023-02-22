dc023a3c8de56db93c484739973916c7b6a2f234
aurore-thomas <aurore.thomas@laplateforme.io>
2023-02-22 22:33:38 +0100

    [ADD] Settings file

---
+++ settings.py
@@ -0,0 +1,30 @@
+import pygame
+
+#------------------------------------
+#          COLORS AND FONTS
+# -----------------------------------
+class Colors :
+    WHITE = (255, 255, 255)
+    BLACK = (25, 25, 25)
+    ORANGE_1 = '#EAA763'
+    ORANGE_2 = '#F8C180'
+    SALMON_1 = '#DE7861'
+    SALMON_2 = '#F2997B'
+    YELLOW_1 = '#F7D567'
+    YELLOW_2 = '#FCE885'
+    GREEN_1 = '#74E86B'
+
+class Fonts :
+    pygame.init()
+    TITLE_FONT = pygame.font.Font('fonts/Blomberg.otf', 90)
+    BUTTON_FONT = pygame.font.Font('fonts/Blomberg.otf', 50)
+    NORMAL_FONT = pygame.font.Font('fonts/Blomberg.otf', 45)
+
+#------------------------------------
+#          FIXED VALUES
+# -----------------------------------
+class Values:
+    SIZE = (1100, 654)
+    FPS = 60
+    CELL_SIZE = 30
+    CELL_NUMBER = 20
