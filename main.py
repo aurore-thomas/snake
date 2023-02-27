fc89222668c6559dbb357b7a6f8442c6a7d43915
aurore-thomas <aurore.thomas@laplateforme.io>
2023-02-27 16:09:26 +0100

    Progress with the snake

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from settings import *
 # ------------------------------------
 #               WINDOW
 # ------------------------------------
-# We define the game window. We can't put all this in a function because of the class Fonts,
-# which need pygame.init() to work. 
 pygame.init()
 clock = pygame.time.Clock()
 win = pygame.display.set_mode(Values.SIZE)
@@ -43,17 +41,6 @@ def button(text, x, y, w, h, ic, ac, action=None):
     text_rect.center = ((x + (w/2)), (y + (h/2))) # To have the text in the middle of the button
     win.blit(text_surf, text_rect)
 
-# ------------------------------------
-#              MAIN MENU
-# ------------------------------------
-def main_menu():
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
 # ------------------------------------
 #              SCORES PAGE
@@ -62,12 +49,72 @@ def scores():
     pass
 
 # ------------------------------------
-#               GAME
+#           INPUT USERNAME
 # ------------------------------------
 def window_game():
     running = True
+    username = ""
 
-    # Background :
+    while running:
+        # Because of the input, we have to draw all the element in the loop,
+        # even the title and the buttons. Otherwise, the background will appear over them.
+
+        # Background :
+        menu_background = pygame.image.load("pictures/background.png")
+        win.blit(menu_background, (0,0))
+
+        # Title :
+        title = Fonts.TITLE_2_FONT.render("SNAKE GAME", True, Colors.ORANGE_2)
+        win.blit(title, (700, 50))
+
+        # Game surface : 
+        game_surface = pygame.draw.rect(win, Colors.BLACK, pygame.Rect(40, 27, Values.CELL_SIZE * Values.CELL_NUMBER, Values.CELL_SIZE * Values.CELL_NUMBER))
+
+        # Text explanation:
+        text_1 = Fonts.NORMAL_FONT.render("Enter your name", True, Colors.WHITE)
+        text_2 = Fonts.NORMAL_FONT.render("and press Enter", True, Colors.WHITE)
+        win.blit(text_1, (709.5, 200))
+        win.blit(text_2, (709.5, 250))
+
+        # Input : 
+        text_surface = Fonts.NORMAL_FONT.render(username, False, Colors.WHITE)
+        win.blit(text_surface, (870 - text_surface.get_width()/2, 320))
+
+        # Buttons : 
+        button("MENU", 770, 450, 200, 70, Colors.ORANGE_2, Colors.ORANGE_1, main)
+        button("QUIT", 770, 550, 200, 70, Colors.SALMON_2, Colors.SALMON_1, quit)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    username = username[:-1]
+                elif event.key == pygame.K_RETURN:
+                    print(username)
+                    play(username)
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                else:
+                    username += event.unicode # To write with the keyboard
+
+        
+
+        pygame.display.update()
+        # clock.tick(Values.FPS)
+
+# ------------------------------------
+#           INPUT USERNAME
+# ------------------------------------
+    
+def play(username):
+    running = True
+    game_over = False
+    score = 0
+
     menu_background = pygame.image.load("pictures/background.png")
     win.blit(menu_background, (0,0))
 
@@ -75,39 +122,30 @@ def window_game():
     title = Fonts.TITLE_2_FONT.render("SNAKE GAME", True, Colors.ORANGE_2)
     win.blit(title, (700, 50))
 
-    # Game surface : 
-    game_surface = pygame.Surface((Values.CELL_SIZE * Values.CELL_NUMBER, Values.CELL_SIZE * Values.CELL_NUMBER))
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        
-        win.blit(game_surface, (40, 27))
-        display_score("0")
-
-        button("PLAY", 770, 450, 200, 70, Colors.ORANGE_2, Colors.ORANGE_1, play)
-        button("MENU", 770, 550, 200, 70, Colors.SALMON_2, Colors.SALMON_1, main)
-
-
-        pygame.display.update()
-        clock.tick(Values.FPS)
-    
-def play():
-    running = True
-    game_over = False
+    # Username display :
+    display_username = Fonts.NORMAL_FONT.render(" PLAYER : " + username + " ", True, Colors.BLACK)
+    pygame.draw.rect(win, Colors.SALMON_1, pygame.Rect(870 - display_username.get_width()/2, 146, display_username.get_width(), 50), border_radius= 10)
+    win.blit(display_username, (870 - display_username.get_width()/2, 150))
 
     # Initial position in the middle of the game surface :
     snake_position = [320, 309] 
+    direction = "west" # Initial move
 
-    # Initial snake has two blocks : 
-    snake = [ [snake_position[0] + Values.CELL_SIZE, snake_position[1] + Values.CELL_SIZE], 
-                [snake_position[0] + Values.CELL_SIZE + (Values.CELL_SIZE), snake_position[1] + Values.CELL_SIZE] ]
-
-    direction = "west" # Initial direction
+    # Fruits : 
+    fruit_apparition = [random.randrange(50, 630), random.randrange(37, 617) ]
+    fruit_is_present = True
 
     while running:
+        snake = [ [snake_position[0], snake_position[1]], 
+                [snake_position[0], snake_position[1]],
+                [snake_position[0], snake_position[1]] ]
+
+        # Display elements :
+        game_surface = pygame.draw.rect(win, Colors.BLACK, pygame.Rect(40, 27, Values.CELL_SIZE * Values.CELL_NUMBER, Values.CELL_SIZE * Values.CELL_NUMBER))
+        button("FINISH", 770, 450, 200, 70, Colors.ORANGE_2, Colors.ORANGE_1)
+        button("MENU", 770, 550, 200, 70, Colors.SALMON_2, Colors.SALMON_1, main)
+        display_score(score)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -117,29 +155,56 @@ def play():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     direction = "north"
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     direction = "south"
-                if event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT:
                     direction = "east"
-                if event.key == pygame.K_LEFT:
+                elif event.key == pygame.K_LEFT:
                     direction = "west"
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
             
         # The snake has to move constantly : 
         if direction == "north":
             snake_position[1] -= Values.CELL_SIZE
-        if direction == "south":
+        elif direction == "south":
             snake_position[1] += Values.CELL_SIZE
-        if direction == "east":
+        elif direction == "east":
             snake_position[0] += Values.CELL_SIZE
-        if direction == "west":
+        elif direction == "west":
             snake_position[0] -= Values.CELL_SIZE
 
+
+        if snake_position[0] < 39 or snake_position[0]>= 619:
+            print("game over")
+            endgame(username, score)
+        elif snake_position[1] <= 28 or snake_position[1] >= 606:
+            print("game over")
+            endgame(username, score)
+
+        # Display the fruit : 
+        pygame.draw.rect(win, Colors.GREEN_1, pygame.Rect(fruit_apparition[0], fruit_apparition[1], 20, 20))
+
+        # When the snake eats the fruit :
+        snake.insert(0, list(snake_position)) 
+        if snake_position[0] == fruit_apparition[0] and snake_position[1] == fruit_apparition[1]:
+            score += 20
+            print("win")
+            
+            fruit_is_present = False
+        # else:
+        #     snake.pop()
+
+
+        # If no fruit on the board : 
+        if fruit_is_present == False:
+            fruit_apparition = [random.randrange(50, 630), random.randrange(37, 617) ]
+
         # Display the snake : 
+        # snake.insert(0, list(snake_position))
         for element in snake:
-            pygame.draw.rect(win, Colors.YELLOW_2, pygame.Rect(element[0], element[1], Values.CELL_SIZE, Values.CELL_NUMBER))
-        
-        if game_over:
-            game_over()
+            pygame.draw.rect(win, Colors.YELLOW_2, pygame.Rect(element[0], element[1], Values.CELL_SIZE, Values.CELL_SIZE))
 
         pygame.display.update()
         clock.tick(Values.SPEED_GAME)
@@ -149,38 +214,42 @@ def play():
 # ------------------------------------
 
 def display_score(actual_score):
-    score = Fonts.NORMAL_FONT.render("Score : " + str(actual_score), True, Colors.BLACK)
-    pygame.draw.rect(win, Colors.SALMON_1, pygame.Rect(770, 300, 200, 50), border_radius= 10)
-    win.blit(score, (783.5, 308))
+    score = Fonts.NORMAL_FONT.render(" Score : " + str(actual_score) + " ", True, Colors.BLACK)
+    pygame.draw.rect(win, Colors.SALMON_1, pygame.Rect(870 - score.get_width()/2, 216, score.get_width(), 50), border_radius= 10)
+    win.blit(score, (870 - score.get_width()/2, 220))
 
 
 # ------------------------------------
 #               ENDGAME
 # ------------------------------------
-def game_over():
-    running= True
+def endgame(username, score):
+    running = True
+    player = username
+    final_score = score
+    print("Endgame")
 
-    # input_rect is the invisble retcangle where the user can write his new word
-    input_rect = pygame.Rect(745, 350, 250, 70)
+    game_over = Fonts.TITLE_FONT.render("GAME OVER", True, Colors.ORANGE_2)
+    game_over_rect = game_over.get_rect(center=(320, 309))
+    win.blit(game_over, game_over_rect)
 
     while running:
-        # We need to create the background and all the elements here for the Backspace
-        # Indeed, if we don't do it, the correction will appear on the previous character
-        word_background = pygame.image.load("pictures/background.png")
-        win.blit(word_background, (0,0))
 
-        game_over = Fonts.TITLE_FONT.render("GAME OVER", True, Colors.ORANGE_2)
-        game_over_rect = game_over.get_rect(center=(320, 309))
-        win.blit(game_over, game_over_rect)
+        button("PLAY AGAIN", 745, 350, 250, 70, Colors.ORANGE_2, Colors.ORANGE_1, window_game)
+        button("MENU", 770, 450, 200, 70, Colors.SALMON_2, Colors.SALMON_1, main)
+        button("QUIT", 770, 550, 200, 70, Colors.SALMON_2, Colors.SALMON_1, quit)
 
-        button("PLAY AGAIN", 745, 450, 250, 70, Colors.ORANGE_2, Colors.ORANGE_1, window_game)
-        button("MENU", 770, 550, 200, 70, Colors.SALMON_2, Colors.SALMON_1, main)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
         
+        pygame.display.update()
 
 
 # ------------------------------------
@@ -210,6 +279,14 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    window_game()
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            
         
         pygame.display.update()
         clock.tick(Values.FPS)
